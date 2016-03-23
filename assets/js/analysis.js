6dc2c2424d1b49548dc31b06c048a39080ae59e9
Unai Zalakain <unai@gisa-elkartea.org>
2016-03-23 16:44:51 +0000

    Do not use ECMA5's `Array.forEach`

--- assets/js/analysis.js
+++ assets/js/analysis.js
@@ -2,12 +2,12 @@
   // Dynamically generate the submenu
   var nav = document.getElementById('play-sections');
   var titles = document.querySelectorAll('article .content h1');
-  Array.forEach(titles, function(title) {
+  for (var i = 0; i < titles.length; i++) {
     var li = document.createElement('li');
     var a = document.createElement('a');
-    a.setAttribute('href', '#' + title.getAttribute('id'));
-    a.innerHTML = title.innerHTML;
+    a.setAttribute('href', '#' + titles[i].getAttribute('id'));
+    a.innerHTML = titles[i].innerHTML;
     li.appendChild(a);
     nav.appendChild(li);
-  });
+  }
 })();
